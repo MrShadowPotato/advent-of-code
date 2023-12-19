aeee56a6ee506fd32de433dc856072b1e665ece4
Sebastian Diaz <sebba.2000@gmail.com>
2023-12-19 13:28:17 -0300

    day-2(part-2)

--- day-2/solution.js
+++ day-2/solution.js
@@ -28,6 +28,34 @@ function partOne() {
   return result;
 }
 
+function partTwo() {
+  let result = 0;
+  for (const line of lines) {
+    const [game, grabs] = line.split(":");
+    let id = parseInt(game.trim().split(" ")[1], 10);
+    console.log("Game with game ", id, " has the following grabs: \n", grabs);
+    let minimun = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+    for (const set of grabs.split(";")) {
+      console.log("set is: ", set);
+      for (const cube of set.trim().split(",")) {
+        console.log("cube is:  ", cube.trim().split(" "));
+        const [num, color] = cube.trim().split(" ");
+        minimun[color] =
+          minimun[color] < parseInt(num) ? parseInt(num) : minimun[color];
+      }
+    }
+    let sum = 1;
+    Object.keys(minimun).forEach((color) => {
+      sum = sum * minimun[color];
+    });
+    result += sum;
+  }
+  return result;
+}
 console.log("First part: ", partOne());
 
-// console.log("Second part: ", partTwo());
+console.log("Second part: ", partTwo());
